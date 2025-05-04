9d43203ee723571eb2f9dd84b29aa5336ffead8b
Brinda873 <brindas149@gmail.com>
2025-05-04 12:55:42 +0530

    Add files via upload

---
+++ database/migrations/2025_03_24_055511_create_purchase_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase', function (Blueprint $table) {
+            $table->id();
+            $table->date('Date');
+            $table->string('Product_name');
+            $table->integer('Quantity');
+            $table->integer('Price');
+            $table->integer('Total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase');
+    }
+};
---
+++ database/migrations/2025_03_24_055527_create_stock_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->id();
+            $table->date('Date');
+            $table->string('Product_name');
+            $table->integer('Quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock');
+    }
+};
